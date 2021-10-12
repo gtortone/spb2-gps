39e2d60be9ff7db6564da3460733596a03ee2d26
Gennaro Tortone <gennaro.tortone@na.infn.it>
2021-10-12 16:03:42 +0200

    add 'state' command and 'all' selector

--- pdu/pdu.py
+++ pdu/pdu.py
@@ -2,6 +2,7 @@
 
 import sys
 import can
+import time
 
 channel = {}
 channel['gps1'] = 7
@@ -11,10 +12,13 @@ channel['hkb']  = 13
 
 if len(sys.argv) > 3:
     busname = sys.argv[1]
-    device = sys.argv[2]
+    if(sys.argv[2] == 'all'):
+        deviceList = ['gps1','gps2','hkb','clkb']
+    else:
+        deviceList = [sys.argv[2]]
     command = sys.argv[3]
 else:
-    print('usage: pdu <bus> <device> on/off')
+    print('usage: pdu <bus> <device> on/off/status')
     print(' bus: {can0, can1}')
     print(' device: { gps1, gps2, hkb, clkb }')
     sys.exit()
@@ -22,26 +26,52 @@ else:
 if (busname.lower() not in ['can0', 'can1']):
     print(f'E: bus {busname} not found')
 
-if (channel.get(device) is None):
-    print(f'E: device {device} not found')
+for device in deviceList:
+    if (channel.get(device) is None):
+        print(f'E: device {device} not found')
+        sys.exit(-1)
+
+if (command.lower() not in ['on', 'off', 'state']):
+    print(f'E: command {command} not valid for device {deviceList}')
     sys.exit(-1)
 
-if (command.lower() not in ['on', 'off']):
-    print(f'E: command {command} not valid for device {device}')
-    sys.exit(-1)
+bus = can.Bus(interface="socketcan", channel=busname, bitrate=250000)
 
-bus = can.Bus(interface="socketcan", channel=busname, bitrate=250000)    ## can0 / can1
+# state
+
+if (command.lower() == 'state'):
+    for device in deviceList:
+        msg = can.Message(arbitration_id=0x18efcf50, data=[0x25, 0x00, channel[device], 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], is_extended_id=True)
+        bus.send(msg)
+        ntry = 0
+        while True:
+            if(ntry == 5):
+                print(f"E: CAN response not received from {busname}")
+                sys.exit(-1)
+            rep = bus.recv(2)   # timeout 3 seconds
+            if rep is not None:
+                arbitrationId = hex(rep.arbitration_id)
+                repid = rep.data[0]
+                repch = rep.data[2]
+                if(repid == 0x26 and repch == channel[device]):
+                    pwstatus = (rep.data[3] & 0x0C) >> 2
+                    print(f"{device} is {'ON' if pwstatus else 'OFF'}")
+                    break
+            ntry = ntry + 1
+    sys.exit(0)        
+
+# power on / power off
 
 if (command.lower() == 'on'):
     power = 1
 elif (command.lower() == 'off'):
     power = 0
 
-# cansend can0 18efcf50#01.40.0C.01.FF.FF.FF.FF
-msg = can.Message(arbitration_id=0x18efcf50, data=[0x01, 0x40, channel[device], power, 0xFF, 0xFF, 0xFF, 0xFF], is_extended_id=True)
+for device in deviceList:
+    msg = can.Message(arbitration_id=0x18efcf50, data=[0x01, 0x40, channel[device], power, 0xFF, 0xFF, 0xFF, 0xFF], is_extended_id=True)
 
-try:
-    bus.send(msg)
-    print('OK')
-except can.CanError:
-    print("E: CAN message not sent")
+    try:
+        bus.send(msg)
+        print(f'{device} OK')
+    except can.CanError:
+        print("E: {device} CAN message not sent")
